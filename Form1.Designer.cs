b45dbc452793ca4f176230c04185378ef021bf6c
kabinet6.5@mail.ru <kabinet6.5@mail.ru>
2021-05-25 13:11:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace ModelBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Addbd = new System.Windows.Forms.Button();
+            this.deletebd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 11);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(601, 361);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Addbd
+            // 
+            this.Addbd.Location = new System.Drawing.Point(12, 378);
+            this.Addbd.Name = "Addbd";
+            this.Addbd.Size = new System.Drawing.Size(172, 58);
+            this.Addbd.TabIndex = 1;
+            this.Addbd.Text = "Добавить";
+            this.Addbd.UseVisualStyleBackColor = true;
+            this.Addbd.Click += new System.EventHandler(this.Addbd_Click);
+            // 
+            // deletebd
+            // 
+            this.deletebd.Location = new System.Drawing.Point(190, 378);
+            this.deletebd.Name = "deletebd";
+            this.deletebd.Size = new System.Drawing.Size(178, 58);
+            this.deletebd.TabIndex = 2;
+            this.deletebd.Text = "Удалить";
+            this.deletebd.UseVisualStyleBackColor = true;
+            this.deletebd.Click += new System.EventHandler(this.deletebd_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Редактировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.deletebd);
+            this.Controls.Add(this.Addbd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Addbd;
+        private System.Windows.Forms.Button deletebd;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using ModelBD.ModelBD;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModelBD
+{
+    public partial class Form1 : Form
+    {
+        ModelBD.Model1 connect = new ModelBD.Model1();
+        private int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connect.Client.Load();
+            dataGridView1.DataSource = connect.Client.Local.ToBindingList();
+        }
+
+        private void Addbd_Click(object sender, EventArgs e)
+        {
+
+            Form2 form = new Form2();
+            DialogResult result = form.ShowDialog(this);
+            if (result == DialogResult.OK)
+            {
+                Client clientadd = new Client();
+                clientadd.FirstName = form.textBox1.Text;
+                clientadd.LastName = form.textBox2.Text;
+                clientadd.Phone = form.textBox3.Text;
+                clientadd.GenderCode = form.comboBox1.Text;
+
+                connect.Client.Add(clientadd);
+                connect.SaveChanges();
+                MessageBox.Show("add");
+
+            }
+        }
+
+        private void deletebd_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+
+            {
+
+                int Index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(dataGridView1[0, Index].Value.ToString(), out id);
+
+
+                if (converted == true)
+
+                {
+
+                    Client ClientDel = connect.Client.Find(id);
+                    connect.Client.Remove(ClientDel);
+                    connect.SaveChanges();
+                    string buff = ClientDel.FirstName;
+                    MessageBox.Show("Запись " + buff + " удалена");
+
+                }
+            }
+            else
+            {
+
+                MessageBox.Show("Строчка не выбрана");
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                Form2 formedit = new Form2();
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+
+                    int idex = dataGridView1.SelectedRows[0].Index;
+                    int id = index;
+                    bool convert = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out id);
+
+
+                    Client clientedit = connect.Client.Find(id);
+
+                    formedit.textBox1.Text = clientedit.FirstName;
+                    formedit.textBox2.Text = clientedit.LastName;
+                    formedit.textBox3.Text = clientedit.Phone;
+                    formedit.comboBox1.Text = clientedit.GenderCode;
+
+                    DialogResult resultedit = formedit.ShowDialog(this);
+                    if (resultedit == DialogResult.OK)
+                    {
+
+                        clientedit.FirstName = formedit.textBox1.Text;
+                        clientedit.LastName = formedit.textBox2.Text;
+                        clientedit.Phone = formedit.textBox3.Text;
+                        clientedit.GenderCode = formedit.comboBox1.SelectedItem.ToString();
+
+                        connect.SaveChanges();
+                        dataGridView1.Refresh();
+                        MessageBox.Show("Объект обновлен");
+                    }
+                }
+            }
+        }
+    }
+}
